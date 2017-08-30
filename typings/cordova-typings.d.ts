eb3bd559c39bd13f708f4a49ec0cf8c1513a5713
prabodh44 <prabodtuladhar44@gmail.com>
2017-08-30 11:03:58 +0545

    Changes before pulling

---
+++ typings/cordova-typings.d.ts
@@ -0,0 +1,5 @@
+
+/// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Camera.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Device.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/FileSystem.d.ts"/>
